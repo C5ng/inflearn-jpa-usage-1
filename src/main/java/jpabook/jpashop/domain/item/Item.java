a3fd0acb1bce1fb6dbd5861f360c492d39889507
공대표 <c5ngsdp@gmail.com>
2024-06-04 21:41:08 +0900

    Section 5. 상품 도메인 개발
    상품 엔티티 개발(비지니스 로직 추가)

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>(); // 다대다 매핑 예제 (실제론 사용 X)
+
+    // 비지니스 로직
+    // 엔티티 자체로 해결 가능하면 엔티티 안에 비지니스 로직을 작성한다. -> 객체지향적
+    // setter를 사용하는 것이 아닌 addStock, removeStock과 같은 메서드를 사용해서 값을 변경시킨다. -> 객체지향적
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
